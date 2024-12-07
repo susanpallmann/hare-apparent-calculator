7b8273955187c25900696c14e001c79a6f6e314a
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2024-12-07 01:14:51 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 var existingHares = 0;
 var enteringHares = 0;
 var tokensMade = 0;
-$('#calculate').click(function(){
-  existingHares = $('#existingHares').val();
-  enteringHares = $('#enteringHares').val();
-  existingHares = +existingHares;
-  enteringHares = +enteringHares;
-  for(i=0;i<enteringHares;i++){
-    tokensMade = tokensMade + existingHares;
-    existingHares++;
-  }
-  $('#huge-answer-number').text(tokensMade);
+$(document).ready(function () {
+  $('#calculate').click(function(){
+    existingHares = $('#existingHares').val();
+    enteringHares = $('#enteringHares').val();
+    existingHares = +existingHares;
+    enteringHares = +enteringHares;
+    for(i=0;i<enteringHares;i++){
+      tokensMade = tokensMade + existingHares;
+      existingHares++;
+    }
+    $('#huge-answer-number').text(tokensMade);
+  });
 });
