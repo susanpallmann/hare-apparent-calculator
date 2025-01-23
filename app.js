c14f4075ab3bd0896000758bf0b4238c55b49bc7
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2025-01-23 11:38:20 -0500

    Update app.js

--- app.js
+++ app.js
@@ -244,7 +244,7 @@ function queueRabbitAnimation(numberRabbits) {
 		
 		// Creates a rabbit div element with an ID# for which rabbit it is, and adds some attributes to handle our animation states. I have a hunch that we don't need the xPos/yPos parts anymore though. TODO
 		// May also refactor my CSS logic to use one attribute for all animation states since my code shouldn't be referencing these anymore
-		const rabbit = $(`<div class="rabbit" id="rabbit${rabbitsDropped}" animation="falling" style="top:${-rabbitHeight}px;left:${xPos}px"></div>`);
+		const rabbit = $(`<div class="rabbit" id="rabbit${rabbitsDropped}" animation="falling" row="${currentRowIndex}" style="top:${-rabbitHeight}px;left:${xPos}px"></div>`);
 		
 		// Adds the created rabbit to the container in the DOM
 		container.prepend(rabbit);
