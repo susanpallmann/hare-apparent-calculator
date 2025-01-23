f27bc4b269dbe153df37951c4d75af9daf479da3
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2025-01-23 10:56:42 -0500

    Update app.js

--- app.js
+++ app.js
@@ -222,6 +222,10 @@ function queueRabbitAnimation(numberRabbits) {
         let xPos = availableXPositions.splice(randomIndex, 1)[0]; // Remove the selected x position
 		xPos -= widthBuffer;
 		let currentRowLength = (rows[currentRowIndex]).length;
+		console.log(containerWidth);
+		console.log(rows[currentRowIndex][currentRowLength]);
+		console.log(rows[currentRowIndex][0]);
+		console.log(rows[currentRowIndex][currentRowLength] - rows[currentRowIndex][0]);
 		xPos += (containerWidth - (rows[currentRowIndex][currentRowLength] - rows[currentRowIndex][0] + rabbitWidth))/2;
 		
 		// Creates a rabbit div element with an ID# for which rabbit it is, and adds some attributes to handle our animation states. I have a hunch that we don't need the xPos/yPos parts anymore though. TODO
