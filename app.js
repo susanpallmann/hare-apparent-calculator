05ac9a841615815ae994d4b8354dc31f224c87ca
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2025-01-24 15:18:30 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // V0.16.1 // Small timing adjustments, removing console logs from debugging
 
 // Global constants
-const gravitySpeed = 0.6; // Controls how long it takes the rabbits fall, a higher number results in slower falling
-const dropRateModifier = 0.1; // Modifies the rate at which rabbits are dropped, a higher number means more time between each rabbit being dropped
+const gravitySpeed = 0.65; // Controls how long it takes the rabbits fall, a higher number results in slower falling
+const dropRateModifier = 0.3; // Modifies the rate at which rabbits are dropped, a higher number means more time between each rabbit being dropped
 const rabbitWidth = 156; // Width of rabbits (must match CSS)
 const rabbitHeight = 114; // Height of rabbits (must match CSS)
 const heightBuffer = rabbitHeight * 0.46; // The amount 2 rabbits should overlap on y axis
