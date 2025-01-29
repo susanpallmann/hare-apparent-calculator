1335d49cc5678a2e5863d933e2161926f67d138d
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2025-01-29 17:04:55 -0500

    Update app.js

--- app.js
+++ app.js
@@ -78,10 +78,10 @@ $(function() {
 			// if there is a timer running, reset it
 			if (timer) {
 				clearInterval(timer);
-				setTimerUI ($loadingBar, 0, rabbits);
+				setTimerUI ($loadingBar, 0, finalRabbits);
 			}
 			
-			setTimerUI ($loadingBar, 100, rabbits);
+			setTimerUI ($loadingBar, 100, finalRabbits);
 			// Once our timer completes...
 			timer = setInterval(function() {
 				// reset the timer
